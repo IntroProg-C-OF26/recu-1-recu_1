18f90e3c6572f15724de98a74bfde56fe63b8202
Danniela19 <danielajaya01@gmail.com>
2026-02-06 12:39:13 -0500

    ejercicios

---
+++ Daniela Jaya/Repaso_RECU_dkjaya/Ejercicio02_IfDoble.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Ejercicio02_IfDoble {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // ENTRADAS (para completar en vivo)
+        System.out.print("Ingrese el total de la compra: ");
+        double totalCompra = sc.nextDouble();
+
+        System.out.print("Ingrese la universidad (UTPL / UNL): ");
+        String universidad = sc.next();
+
+        // PROCESO (IF DOBLE)
+        double descuento = 0.0;
+
+        if (universidad.equals("UTPL")) {
+            descuento = 0.10;
+        } else {
+            descuento = 0.15;
+        }
+        
+        totalCompra = totalCompra - (totalCompra * descuento);
+        System.out.println("Descuento y Compra: (" + descuento + "), ($" + totalCompra + ")");
+        // SALIDA
+        // TODO: mostrar universidad, descuento aplicado y total a pagar
+
+        sc.close();
+    }
+}
