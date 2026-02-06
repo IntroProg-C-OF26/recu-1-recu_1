bc6113343ed0bada6f96644f53cb0bf167e8923f
carolina10134 <ccarvallo@utpl.edu.ec>
2026-02-06 12:14:20 -0500

    ...

---
+++ Carolina Carvallo/Repaso_RECU_carolina10134/src/Ejercicio02_DescuentoCafeteriaIfDoble.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+
+/**
+ *En una cafetería, si el cliente es estudiante de la UTPL recibe 10% de descuento; pero si es de la UNL su descuento es del 15%.
+ * @author carolina carvallo
+ */
+public class Ejercicio02_DescuentoCafeteriaIfDoble {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // ENTRADAS (para completar en vivo)
+        System.out.print("Ingrese el total de la compra: ");
+        double totalCompra = sc.nextDouble();
+
+        System.out.print("Ingrese la universidad (UTPL / UNL): ");
+        String universidad = sc.next();
+
+        // PROCESO (IF DOBLE)
+        double descuento = 0.0;
+
+        if (universidad.equals("UTPL")) {
+            descuento = 0.10;
+        } else {
+            descuento = 0.15;
+        }
+
+        totalCompra = totalCompra - (totalCompra * descuento);
+        
+        System.out.println(" descuento y compra: ("+ descuento + "), (" + totalCompra + ")");
+        sc.close();
+    }
+}
+/**run:
+Ingrese el total de la compra: 100
+Ingrese la universidad (UTPL / UNL): UTPL
+ descuento y compra: (0.1), (90.0)
+BUILD SUCCESSFUL (total time: 5 seconds)
