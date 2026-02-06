8402328906d9e36fcc6dc4606347734b4fcdf4a9
Toto19006 <toto18200617@hotmail.com>
2026-02-06 13:03:43 -0500

    Tres ejercicios Tomás

---
+++ Tomás Herrera/REPASO_RECU_pdTOTO/src/Ejercicio03_TipoDiaIFMultiple.java
@@ -0,0 +1,28 @@
+/**
+ * Un sistema clasifica el día según la temperatura: frío (≤15 °C), templado 
+ * (16–25 °C) o caluroso (≥26 °C).
+ * @author Toto1
+ */
+import java.util.Random;
+public class Ejercicio03_TipoDiaIFMultiple {
+
+    public static void main(String[] args) {
+        Random ale = new Random();
+        int temperatura = ale.nextInt(40); // TODO: asignar valor
+
+        if (temperatura <= 15 ) {
+            System.out.println("FRIO: " + temperatura + " o");
+        } else if (temperatura <= 25) {
+            System.out.println("TEMPLADO: " + temperatura + " o");
+        } else {
+            System.out.println("CALUROSO: " + temperatura + " o");
+        }
+
+    }
+}
+/***
+ * run: 
+ * CALUROSO: 27 o
+ * FRIO: 6 o
+ * TEMPLADO: 20 o
+ */
