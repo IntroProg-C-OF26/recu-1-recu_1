af22aecd03238a73e33da8db2b230113398177b1
Judavi-baby <zeialf123@gmail.com>
2026-02-06 12:29:49 -0500

    xdxddxd

--- Julian Muñoz/Repaso_RECU_Judavi-baby/src/Ejercicio03_TipoDia.java
+++ Julian Muñoz/Repaso_RECU_Judavi-baby/src/Ejercicio03_TipoDia.java
@@ -1,12 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
+import java.util.Random;
 
 /**
- *
- * @author julia
+ * Un sistema clasifica el día según la temperatura: frío (≤15 °C), templado (16–25 °C) o caluroso (≥26 °C).
+ * @author julian muñoz
  */
 public class Ejercicio03_TipoDia {
-    
+    public static void main(String[] args) {
+        Random ale = new Random();
+        int temperatura = ale.nextInt(40);
+        
+        if (temperatura <= 15){
+            System.out.println("Frio: " + temperatura);
+        } else if (temperatura <= 25) {
+            System.out.println("Templado: " + temperatura);
+        } else {
+            System.out.println("Caluroso: " + temperatura);
+        }
+        
+        
+    }
 }
